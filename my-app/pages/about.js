eace8025cf71c9aa094b250486b1cd3c6883540e
ravigordon <rgordz460@outlook.com>
2022-03-07 12:13:01 +0000

    latest mobile responsive

--- my-app/pages/about.js
+++ my-app/pages/about.js
@@ -104,8 +104,8 @@ function About() {
             id={css.image}
             src={Farmwaste}
             alt="Card image cap"
-            width={800}
-            height={700}
+            width={900}
+            height={800}
           ></Image>
           <div className={css.textContent}>
             <h2 className={css.contentTitle}>
